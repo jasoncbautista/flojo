4a89712a8043dbab9148502aaab2b89d154fbdd7
Jason B <bjason@gmail.com>
2013-11-11 21:28:08 -0800

    Read file

--- nodeTests/readFile.js
+++ nodeTests/readFile.js
@@ -1,3 +1,9 @@
+var _ = require("underscore");
 var fs = require("fs");
 var file = fs.readFileSync("template.html", "utf8");
-console.log(file);
+// console.log(file);
+var compiledTemplate = _.template(file)({name: "Names name"});
+
+console.log(compiledTemplate);
+
+
