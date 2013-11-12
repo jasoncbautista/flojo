6a93438d359d272a1a8484a23ccaf51c26578225
Jason B <bjason@gmail.com>
2013-11-11 21:18:14 -0800

    Server api get point

--- server.js
+++ server.js
@@ -5,4 +5,8 @@ app.get('/', function(req, res){
   res.send('Hello World');
 });
 
+app.get("/api/v1/entities/create/:name", function(req, res) {
+
+    console.log("name", req.params.name);
+});
 app.listen(3091);
