a81a5f3745be6936164f4161d01c3ec435ec9a2e
Jason B <bjason@gmail.com>
2013-11-11 21:31:19 -0800

    remove console

--- nodeTests/readFile.js
+++ nodeTests/readFile.js
@@ -1,7 +1,6 @@
 var _ = require("underscore");
 var fs = require("fs");
 var file = fs.readFileSync("template.html", "utf8");
-// console.log(file);
 var compiledTemplate = _.template(file)({name: "Names name"});
 
 console.log(compiledTemplate);
